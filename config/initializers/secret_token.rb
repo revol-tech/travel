faa20d403d809d90c1dac836478c18ee9a2bc7d4
xecutioner <kapilnakhwa@gmail.com>
2013-03-14 23:55:45 +0545

    initial payload

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Footprints::Application.config.secret_token = '55a836a45ed8eba1d1c197a213ec7f985ad7aa6b2482d95020baac4813348e94c33500c9ab105d06698f025c3858a2234a87c1fa8d709f51c7dee41e97f8a64c'
